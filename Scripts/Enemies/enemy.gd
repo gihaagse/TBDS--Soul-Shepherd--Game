a206c954c4f0e59b59dbd8a54b48582023085ceb
DerksenLucas <23111186@student.hhs.nl>
2025-10-27 14:51:37 +0100

    enemy patch 1

--- Scripts/Enemies/enemy.gd
+++ Scripts/Enemies/enemy.gd
@@ -14,6 +14,9 @@ var player: CharacterBody2D = null
 @export var playerInRange: bool = false
 @export var flippedSprite: bool = false
 @export var shotOffset: int = 15
+@onready var ground: RayCast2D = $GroundCheck
+@onready var gravity: float = 300.0
+var ground_collider
 
 
 var old_hp : int
@@ -25,22 +28,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * _delta
+	else:
+		velocity.y = 0
+	
 	if old_hp != health.hp:
 		label.text = "HP: " + str(health.hp)
 		old_hp = health.hp
 		
-	if is_on_wall():
+	if is_on_wall() or !ground.is_colliding():
 		dir = dir * -1
 		_correct_sprite()
+		ground.position.x = ground.position.x * dir
 
 	position.x += speed * dir * _delta
 	move_and_slide()
 	if playerInRange:
-		if (player.position.x - (position.x+150)) > 0:
+		if (player.position.x - position.x) > 0:
 			dir = 1
 			flippedSprite = false
 			_correct_sprite()
-		elif (player.position.x - (position.x+150)) < 0:
+		elif (player.position.x - position.x) < 0:
 			dir = -1
 			flippedSprite = true
 			_correct_sprite()
