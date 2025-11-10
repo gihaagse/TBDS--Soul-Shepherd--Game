77aa16f13a5a98c94961e5413a4d7a7535ef6652
gihaagse <gihaagse@gmail.com>
2025-11-10 12:03:17 +0100

    Attack damage fix

--- Scripts/Weapons/melee_weapon.gd
+++ Scripts/Weapons/melee_weapon.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @export var sprite : AnimatedSprite2D
-@export var animation_name: String
+@export var anim_player : AnimationPlayer
+@export var animation_name_left: String = "AttackAnimation_Left"
+@export var animation_name_right: String = "AttackAnimation_Right"
 @export var frame_hit: Array[int]
 @export var damage : int
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
@@ -12,7 +14,7 @@ func _ready() -> void:
 	collision_shape_2d.disabled = true
 
 func _process(_delta: float) -> void:
-	if sprite.animation == animation_name and sprite.frame in frame_hit:
+	if anim_player.current_animation == animation_name_left or animation_player.current_animation == animation_name_right and sprite.frame in frame_hit:
 		collision_shape_2d.disabled = false
 		audio_stream_player_2d.playing = true
 	else:
@@ -22,7 +24,7 @@ func _process(_delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	print("Hit!")
-	var hp = area.get_node_or_null("Health")
+	var hp = area.get_parent().get_node_or_null("Health")
 	if hp and hp.has_method("take_damage"):
 		print("Working_code :D")
 		hp.take_damage(damage)
