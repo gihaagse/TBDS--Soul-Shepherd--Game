79a84cf6a9c344649b6b76dcd25a8dd35aedb85d
gihaagse <gihaagse@gmail.com>
2025-12-11 00:30:24 +0100

    feat(ux)can now use dpad in vertical options

--- Scripts/UI/GameMenus/start_menu.gd
+++ Scripts/UI/GameMenus/start_menu.gd
@@ -3,13 +3,13 @@ extends Control
 @onready var options_menu: OptionsMenu = $OptionsMenu as OptionsMenu
 @onready var v_box_container: VBoxContainer = $VBoxContainer as VBoxContainer
 
-
+var _last_focused: Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	options_menu.exit_options_menu.connect(on_exit_options_menu)
 	v_box_container.visible = true
-	$VBoxContainer/Start.grab_focus()
 	OptionsManager._set_focus_all_on_children(self)
+	$VBoxContainer/Start.call_deferred("grab_focus")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -35,12 +35,20 @@ func on_exit_options_menu() -> void:
 	options_menu.set_process(false)
 	options_menu.visible = false
 	$Logo.visible = true
+	regain_menu_focus()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 	
+func _on_any_button_focus_entered():
+	_last_focused = get_viewport().gui_get_focus_owner()
 
-
+func regain_menu_focus():
+	if _last_focused and is_instance_valid(_last_focused):
+		_last_focused.call_deferred("grab_focus")
+	else:
+		$VBoxContainer/Start.call_deferred("grab_focus")
+		
 func _on_game_start_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Level/playtest_level.tscn")
 	Engine.time_scale = 1
