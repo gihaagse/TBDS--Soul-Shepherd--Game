cbeaf663c45797718360413523ad6f1f6f8a3e94
JayUndrScr <jayjadi1406@gmail.com>
2025-12-10 16:00:30 +0100

    Plus level 1

--- Scenes/Level/next_level.gd
+++ Scenes/Level/next_level.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@export var level_name : String
+
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level/playtest2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level/" + str(level_name) + ".tscn")
 	CheckPointManager.reset_checkpoints()
